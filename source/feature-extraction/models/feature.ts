7d28e5b4f7318846f01b9b9c0128a2665d2469db
onwd <onwdit@gmail.com>
2020-12-26 02:42:44 +0600

    Update Feature model

--- source/feature-extraction/models/feature.ts
+++ source/feature-extraction/models/feature.ts
@@ -2,11 +2,16 @@ import { FeatureType } from '../enums';
 
 export class Feature {
   public type: FeatureType;
-  public probability?: number;
-  public reason?: string;
-  public error?: any;
+  public probability: number | null;
+  public reason: string | null;
+  public error: Error | null;
+  public timeTaken: number;
 
   constructor(feature: Partial<Feature>) {
-    Object.assign(this, feature);
+    this.type = feature.type;
+    this.probability = feature.probability ?? null;
+    this.reason = feature.reason ?? null;
+    this.error = feature.error ?? null;
+    this.timeTaken = feature.timeTaken;
   }
 }
