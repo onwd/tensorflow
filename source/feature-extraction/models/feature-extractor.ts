2e560b4a86dce1ebdcbdacf04936bf7626723020
onwd <onwdit@gmail.com>
2021-02-18 19:33:18 +0600

    Make FeatureExtractor generic

--- source/feature-extraction/models/feature-extractor.ts
+++ source/feature-extraction/models/feature-extractor.ts
@@ -1,6 +1,6 @@
 import { Feature } from './feature';
 import { FeatureExtractorSharedData } from './feature-extractor-shared-data';
 
-export abstract class FeatureExtractor {
-  public abstract extract(data: any, sharedData: FeatureExtractorSharedData): Feature | Promise<Feature>;
+export abstract class FeatureExtractor<T> {
+  public abstract extract(data: T, sharedData: FeatureExtractorSharedData): Feature | Promise<Feature>;
 }
